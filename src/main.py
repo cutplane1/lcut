1e386e5b6a2c6ece804e4ea47fb922821c13f0de
cutplane1 <snakesnake222@mail.ru>
2023-05-17 19:48:41 +0500

    redirect

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 import database, linkgen, config
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
-db = database.load(config.database_file)
+db = database.load(config.DATABASE_FILE)
 
 @app.route("/")
 def main_page():
     return render_template('index.html')
+
+@app.route("/<string:key>")
+def redirect_page(key):
+    return redirect("https://" + database.search_link(db, key))
 
 @app.route("/s", methods=['POST'])
 def save_page():
     sym = linkgen.generate_random_symbols(8)
 
     database.append(db, sym, request.form['link'])
-    database.write(config.database_file, db)
+    database.write(config.DATABASE_FILE, db)
 
     return config.URL + "/" + sym
