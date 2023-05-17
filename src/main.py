dddb1d27a38cbdd6c3ccf62ef6d6c798f41e8b97
cutplane1 <snakesnake222@mail.ru>
2023-05-17 19:00:37 +0500

    example update

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ db = database.load(config.database_file)
 
 print(database.search_link(db, "oneb"))
 
-# database.append(db, linkgen.generate_random_symbols(8), "ww.com")
+database.append(db, linkgen.generate_random_symbols(8), "ww.com")
 
 database.write(config.database_file, db)
