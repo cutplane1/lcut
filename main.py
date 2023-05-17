a7f7b34542b98406c2d11299447318afd688f0f8
cutplane1 <snakesnake222@mail.ru>
2023-05-17 18:10:11 +0500

    db

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ def load():
         return json.load(json_file)
 
 
-def write():
+def write(db):
     with open("db.json", "w") as json_file:
         json.dump(db, json_file, sort_keys=True, indent=2)
 
 
 db = load()
+
+print(db)
 
 # db.append({"file_path": "C:\\folder\\pic.png", "tags": ["cat", "kot"]})
 
