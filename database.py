f7089ad4898c7e3684c29472adf121fa8ac66367
cutplane1 <snakesnake222@mail.ru>
2023-05-17 18:34:42 +0500

    separating package

---
+++ database.py
@@ -0,0 +1,18 @@
+import json
+
+def load():
+    with open("db.json") as json_file:
+        return json.load(json_file)
+
+
+def write(db):
+    with open("db.json", "w") as json_file:
+        json.dump(db, json_file, sort_keys=True, indent=2)
+
+
+def append(db, key, link):
+    db[key] = link
+
+
+def search_link(db, key):
+    return db[key]
--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-import json
+import database
 
-def load():
-    with open("db.json") as json_file:
-        return json.load(json_file)
+db = database.load()
 
+database.search_link(db, "oneb")
 
-def write(db):
-    with open("db.json", "w") as json_file:
-        json.dump(db, json_file, sort_keys=True, indent=2)
+# database.append(db, "ttb", "ww.com")
 
-
-def append(db, key, link):
-    db[key] = link
-
-
-def search_link(db, key):
-    return db[key]
-
-
-db = load()
-
-search_link(db, "oneb")
-
-append(db, "ttb", "ww.com")
-
-write(db)
+database.write(db)
