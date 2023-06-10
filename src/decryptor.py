1f426ca2b02a8181cd42537a6f8657f0ed870832
cutplane1 <snakesnake222@mail.ru>
2023-06-10 15:01:07 +0500

    fix

--- src/decryptor.py
+++ src/decryptor.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen, Request
 
 def get_final_link(url):
-    if url.startswith("http") or url.startswith("http"):
+    if not url.startswith("http") or not url.startswith("http"):
         url = "https://" + url
     
     httprequest = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
