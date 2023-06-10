32284a970513f3a6a2fea562ae15788f7c62a4c4
cutplane1 <snakesnake222@mail.ru>
2023-06-10 14:38:44 +0500

    _

--- src/linkgen.py
+++ src/linkgen.py
@@ -1,4 +1,4 @@
 import random, string
 
 def generate_random_symbols(l): 
-    return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(l)])
+    return ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(l)])
